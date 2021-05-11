12cc2f3d95676bff61bb419557c8ab71d642ba09
Jack Lee <57369978+JackJackJ@users.noreply.github.com>
2021-05-11 11:57:55 -0500

    opium event

--- events.js
+++ events.js
@@ -5,7 +5,6 @@ function execute()
     var crew = JSON.parse(localStorage.getItem("crew"));
     crew.splice(localStorage.getItem("position"), 1);
     localStorage.setItem("crew", JSON.stringify(crew));
-    localStorage.setItem("morale", localStorage.getItem("morale")-10);
 }
 function newCrew(action)
 {
@@ -48,7 +47,7 @@ function disease(action)
         crew.splice(Math.floor(Math.random()*crew.length),1);
         crew.splice(Math.floor(Math.random()*crew.length),1);
         localStorage.setItem("crew", JSON.stringify(crew));
-        localStorage.setItem("morale", localStorage.getItem("morale")-5);
+        localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-5);
         document.getElementById('disease').innerHTML = "Your actions get the outbreak mostly under control, but some still perish of the disease.";
     }
     if(action == "denial")
@@ -62,12 +61,13 @@ function disease(action)
         crew.splice(Math.floor(Math.random()*crew.length),1);
         crew.splice(Math.floor(Math.random()*crew.length),1);
         localStorage.setItem("crew", JSON.stringify(crew));
-        localStorage.setItem("morale", localStorage.getItem("morale")-10);
+        localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-10);
         document.getElementById('disease').innerHTML = "The disease ravages the ship and leaves much of your crew dead. The survivors' faith in your leadership is greatly shaken.";
     }
     if(action == "plank")
     {
         execute();
+        localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-10);
         document.getElementById('disease').innerHTML = "No man dares cough in your presence again.";
     }
 }
@@ -76,19 +76,29 @@ function opium(action)
     if(action == "overboard")
     {
         localStorage.setItem("event", "passed");
-        localStorage.setItem("morale", localStorage.getItem("morale")-1);
+        localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-1);
         document.getElementById('opium').innerHTML = "Some of your men grumble. They ought to be grateful they aren't walking the plank.";
     }
     if(action == "libertarian")
     {
-        localStorage.setItem("morale", localStorage.getItem("morale")+10);
-        for(i = 1; i < Math.ceil(Math.random()*3); i++)
+        localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))+10);
+        var crew = JSON.parse(localStorage.getItem("crew"));
+        for(i = 0; i < Math.ceil(Math.random()*5); i++)
         {
             localStorage.setItem("event", "passed");
-            var crew = JSON.parse(localStorage.getItem("crew"));
-            crew[Math.floor(Math.random()*crew.length)]["fighting"] = crew[Math.floor(Math.random()*crew.length)]["fighting"]*.5;
-            crew[Math.floor(Math.random()*crew.length)]["piloting"] = crew[Math.floor(Math.random()*crew.length)]["piloting"]*.5;
+            var position = Math.floor(Math.random()*crew.length);
+            crew[position]["fighting"] = Math.floor(parseInt(crew[position]["fighting"])*.5);
+            crew[position]["piloting"] = Math.floor(parseInt(crew[position]["piloting"])*.5);
         } 
+        localStorage.setItem("crew", JSON.stringify(crew));
         document.getElementById('opium').innerHTML = "A good deal of your men wind up getting hooked on opium. While the addicts are substantially worse at being pirates, they are at least easier to order around";
+    }
+    if(action == "confiscate")
+    {
+        localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-3);
+        var inventory = JSON.parse(localStorage.getItem("inventory"));
+        inventory["opium"] = inventory["opium"] + Math.floor(Math.random()*10);
+        localStorage.setItem("inventory", JSON.stringify(inventory));
+        document.getElementById('opium').innerHTML = "Much to your mens' dismay, you confiscate the opium. It ought to fetch a good profit at the next port.";
     }
 }
