25acd3fff9b3cce7973ca886adc0f717e0cf855f
Jack Lee <57369978+JackJackJ@users.noreply.github.com>
2021-05-11 12:47:12 -0500

    started fighting

--- events.js
+++ events.js
@@ -76,16 +76,18 @@ function opium(action)
     if(action == "overboard")
     {
         localStorage.setItem("event", "passed");
+        activeEvent = false;
         localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-1);
         document.getElementById('opium').innerHTML = "Some of your men grumble. They ought to be grateful they aren't walking the plank.";
     }
     if(action == "libertarian")
     {
         localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))+10);
+        localStorage.setItem("event", "passed");
+        activeEvent = false;
         var crew = JSON.parse(localStorage.getItem("crew"));
         for(i = 0; i < Math.ceil(Math.random()*5); i++)
         {
-            localStorage.setItem("event", "passed");
             var position = Math.floor(Math.random()*crew.length);
             crew[position]["fighting"] = Math.floor(parseInt(crew[position]["fighting"])*.5);
             crew[position]["piloting"] = Math.floor(parseInt(crew[position]["piloting"])*.5);
@@ -95,10 +97,114 @@ function opium(action)
     }
     if(action == "confiscate")
     {
+        localStorage.setItem("event", "passed");
+        activeEvent = false;
         localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-3);
         var inventory = JSON.parse(localStorage.getItem("inventory"));
-        inventory["opium"] = inventory["opium"] + Math.floor(Math.random()*10);
+        inventory["Opium"] = inventory["Opium"] + Math.ceil(Math.random()*10);
         localStorage.setItem("inventory", JSON.stringify(inventory));
         document.getElementById('opium').innerHTML = "Much to your mens' dismay, you confiscate the opium. It ought to fetch a good profit at the next port.";
     }
+    if(action == "plank")
+    {
+        execute();
+        localStorage.setItem("morale", parseInt(localStorage.getItem("morale"))-8);
+        document.getElementById('opium').innerHTML = "You enforce a strict no drug policy on the ship.";
+    }
+}
+
+
+
+
+//fighting functions
+function pirates(action, enemyFighting, enemySailing)
+{
+    if(action == "fight")
+    {
+        if(battle(enemyFighting))
+        {
+            alert("you win!")
+        }
+        else
+        {
+            alert("you lose!")
+        }
+    }
+}
+function getCrew()
+{
+    return JSON.parse(localStorage.getItem("crew"));
+}
+
+function setCrew(crew)
+{
+    localStorage.setItem("crew", JSON.stringify(crew));
+}
+
+function getPlayerFightingStat() // adds the fighting stats of all of the player's crewmates
+{
+    var crew = getCrew();
+    var total = 0;
+    for (var i = 0; i < crew.length; i++)
+    {
+        var crewmate = crew[i];
+        var fightingStat = crewmate["fighting"];
+        total += fightingStat;
+    }
+    return total;
+}
+
+function getPlayerPilotingStat() // adds the fighting stats of all of the player's crewmates
+{
+    var crew = getCrew();
+    var total = 0;
+    for (var i = 0; i < crew.length; i++)
+    {
+        var crewmate = crew[i];
+        var pilotingStat = crewmate["piloting"];
+        total += pilotingStat;
+    }
+    return total;
+}
+
+/*
+* Initiates a battle between the player and an enemy ship
+* Chance of winning is based on the fighting stat of the player and the enemy
+* Returns true if player wins the battle, false otherwise
+*/
+
+function getChance(playerStat, enemyStat)
+{
+    return playerStat/(playerStat+enemyStat);
+}
+
+function battle(enemyFightingStat)
+{
+    var playerFightingStat = getPlayerFightingStat();
+    var sumStat = playerFightingStat + enemyFightingStat;
+    
+    var playerChance = playerFightingStat*1.0 / sumStat;
+    var rand = Math.random();
+
+    if (rand < playerChance) // player wins
+    {
+        return true;
+    }
+    return false;
+}
+
+// same as battle but for piloting
+function escape(enemyPilotingStat)
+{
+    var playerPilotingStat = getPlayerPilotingStat();
+    var sumStat = playerPilotingStat + enemyPilotingStat;
+    
+    var playerChance = playerPilotingStat*1.0 / sumStat;
+    var rand = Math.random();
+
+    if (rand < playerChance) // either player escapes or enemy does not escape (enemy has to fight) depending on the situation
+    {
+        return true;
+    }
+    return false; // either player does not escape (player has to fight) or enemy escapes depending on the situation
 }
